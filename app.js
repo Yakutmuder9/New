e24dda175248a0dddd89ba6b0d0e5bcfb59b2e3a
YAKUT AHMEDIN <yakutmuder9@gmail.com>
2023-03-19 19:48:44 -0400

    week-1

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+        title: "WEB 420 Restful APIs",
+        version: "1.0.0",
+      description: "Customer API Information",
+      contact: {
+        name: "Amazing Developer",
+      },
+    },
+  },
+  apis: ["app.js"],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Application started and listening on port ${port}!`);
+});
